b840845430bf397466104f9cfafeff3123b10dbb
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-06-30 11:59:46 -0400

    removed unnecessary comment

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'ndfoivsorio43i5j4ijtvoi34lkmslfd',
 
-    //what are the || cases for these?
     DATABASE_URL:
         process.env.DATABASE_URL || 'postgresql://traveler_admin:aksdhfj823@localhost/traveler',
     TEST_DATABASE_URL:
